d7519a1ecd4aff3371d78150688ba57c659baf86
Meighan Rasley <meighanr@gmail.com>
2015-12-30 23:31:36 -0800

    Password confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
-  
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true
+
   has_many :owners
   has_many :walkers
 end
